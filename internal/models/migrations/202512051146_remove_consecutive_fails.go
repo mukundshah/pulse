1dd6d11db2ca3ae5d35ce069cb9db939883e24a2
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-12-07 02:44:56 +0545

    refactor: comment out migration for consecutive_fails column in check model

--- internal/models/migrations/202512051146_remove_consecutive_fails.go
+++ internal/models/migrations/202512051146_remove_consecutive_fails.go
@@ -3,18 +3,18 @@ package migrations
 import (
 	"github.com/go-gormigrate/gormigrate/v2"
 	"gorm.io/gorm"
-
-	"pulse/internal/models"
 )
 
 func init() {
 	RegisterMigration(&gormigrate.Migration{
 		ID: "202512051146_remove_consecutive_fails",
 		Migrate: func(tx *gorm.DB) error {
-			return tx.Migrator().DropColumn(&models.Check{}, "consecutive_fails")
+			// return tx.Migrator().DropColumn(&models.Check{}, "consecutive_fails")
+			return nil
 		},
 		Rollback: func(tx *gorm.DB) error {
-			return tx.Migrator().AddColumn(&models.Check{}, "consecutive_fails")
+			// return tx.Migrator().AddColumn(&models.Check{}, "consecutive_fails")
+			return nil
 		},
 	})
 }
