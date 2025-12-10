9bad21252ac4e5902c1e85495a85a67adcc7ce08
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-12-10 11:16:08 +0545

    feat: add loading state support to Button components

--- web/components/ui/button/Button.vue
+++ web/components/ui/button/Button.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import type { PrimitiveProps } from 'reka-ui'
-import type { HTMLAttributes } from 'vue'
+import type { ButtonHTMLAttributes, HTMLAttributes } from 'vue'
 import type { ButtonVariants } from '.'
 
 import { Primitive } from 'reka-ui'
 
+import { Spinner } from '@/components/ui/spinner'
 import { cn } from '@/utils/style'
 import { buttonVariants } from '.'
 
@@ -12,14 +13,37 @@ interface Props extends PrimitiveProps {
   variant?: ButtonVariants['variant']
   size?: ButtonVariants['size']
   class?: HTMLAttributes['class']
+  onClick?: (event: MouseEvent) => void
+  loading?: boolean
+  loadingAuto?: boolean
+  type?: ButtonHTMLAttributes['type']
+  disabled?: boolean
 }
 
 const props = withDefaults(
   defineProps<Props>(),
   {
     as: 'button',
+    loading: false,
+    type: 'button',
   },
 )
+
+const loadingAutoState = ref(false)
+
+const isLoading = computed(() => {
+  return props.loading || (props.loadingAuto && loadingAutoState.value)
+})
+
+const onClickWrapper = async (event: MouseEvent) => {
+  loadingAutoState.value = true
+  const callbacks = Array.isArray(props.onClick) ? props.onClick : [props.onClick]
+  try {
+    await Promise.all(callbacks.map(fn => fn?.(event)))
+  } finally {
+    loadingAutoState.value = false
+  }
+}
 </script>
 
 <template>
@@ -28,7 +52,13 @@ const props = withDefaults(
     :as="as"
     :as-child="asChild"
     :class="cn(buttonVariants({ variant, size }), props.class)"
+    :disabled="props.disabled || isLoading"
+    @click="onClickWrapper"
   >
-    <slot></slot>
+    <slot name="loading">
+      <Spinner v-if="isLoading" />
+    </slot>
+    <slot>
+    </slot>
   </Primitive>
 </template>
