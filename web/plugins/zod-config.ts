0c9ecd3474f874d4c92935e8cde07ff27775f289
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-12-12 12:55:32 +0545

    chore(web): remove debug console.log from zod-config plugin

--- web/plugins/zod-config.ts
+++ web/plugins/zod-config.ts
@@ -4,7 +4,6 @@ import { z } from 'zod'
 export default defineNuxtPlugin(() => {
   z.config({
     customError: (iss) => {
-      console.log(iss)
       if (iss.input === undefined && iss.code === 'invalid_type') {
         return `${titleCase(iss.path?.join('.') ?? 'This field')} is required`
       }
