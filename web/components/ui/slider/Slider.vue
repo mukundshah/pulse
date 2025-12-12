0a08166deaeabce48de052121245c00461cf50bf
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-12-13 00:46:01 +0545

    feat(web): add tick marks support to Slider component

--- web/components/ui/slider/Slider.vue
+++ web/components/ui/slider/Slider.vue
@@ -7,10 +7,10 @@ import { SliderRange, SliderRoot, SliderThumb, SliderTrack, useForwardPropsEmits
 
 import { cn } from '@/utils/style'
 
-const props = defineProps<SliderRootProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<SliderRootProps & { class?: HTMLAttributes['class'], ticks?: number[] | Record<number, string>, max?: number }>()
 const emits = defineEmits<SliderRootEmits>()
 
-const delegatedProps = reactiveOmit(props, 'class')
+const delegatedProps = reactiveOmit(props, 'class', 'ticks')
 
 const forwarded = useForwardPropsEmits(delegatedProps, emits)
 </script>
@@ -26,19 +26,38 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
     v-bind="forwarded"
   >
     <SliderTrack
-      class="bg-muted relative grow overflow-hidden rounded-full data-[orientation=horizontal]:h-1.5 data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-1.5"
+      as="div"
+      class="bg-muted relative grow overflow-visible rounded-full mb-6 data-[orientation=horizontal]:h-1.5 data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-1.5"
       data-slot="slider-track"
     >
       <SliderRange
-        class="bg-primary absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full"
+        as="div"
+        class="bg-primary absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full transition-[width,height] duration-500"
         data-slot="slider-range"
       />
+
+      <template v-if="ticks">
+        <template v-for="(tick, index) in ticks" :key="index">
+          <div
+            class="absolute top-0 right-0"
+            :style="{ right: `${100 - (typeof tick === 'number' ? tick : index / (max ?? 1)) * 100}%` }"
+          >
+            <div class="relative">
+              <div class="absolute top-0 -translate-x-1/2 size-1.5 bg-primary rounded-full"></div>
+              <span class="absolute top-4 -translate-x-1/2 text-xs text-foreground">
+                {{ tick }}
+              </span>
+            </div>
+          </div>
+        </template>
+      </template>
     </SliderTrack>
 
     <SliderThumb
       v-for="(_, key) in modelValue"
       :key="key"
-      class="bg-white border-primary ring-ring/50 block size-4 shrink-0 rounded-full border shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50"
+      as="div"
+      class="bg-white border-primary ring-ring/50 block size-4 shrink-0 rounded-full border shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50 -top-1.25"
       data-slot="slider-thumb"
     />
   </SliderRoot>
