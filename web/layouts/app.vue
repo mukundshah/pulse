0f3a82f336936bdeb0d3ad397cb1f81b4e4617dc
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-12-14 14:28:18 +0545

    style(web): add backdrop blur to header

--- web/layouts/app.vue
+++ web/layouts/app.vue
@@ -199,7 +199,7 @@ const handleLogout = async () => {
         </SidebarFooter>
       </Sidebar>
       <SidebarInset class="overflow-auto h-svh">
-        <header class="bg-background/90 sticky top-0 z-10 flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+        <header class="bg-background/90 sticky top-0 z-10 flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height) backdrop-blur-md">
           <div class="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
             <SidebarTrigger />
           </div>
