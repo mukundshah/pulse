8969bbabf549d795aa5e73e12b1ae17b67257f06
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-12-17 01:10:50 +0545

    refactor(web): remove unused IP_VERSION_LABELS import and simplify IP version display

--- web/pages/(app)/projects/[projectId]/checks/[checkId]/runs/[runId]/index.vue
+++ web/pages/(app)/projects/[projectId]/checks/[checkId]/runs/[runId]/index.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { ASSERTION_PROPERTIES, IP_VERSION_LABELS } from '@/constants/http'
+import { ASSERTION_PROPERTIES } from '@/constants/http'
 import { constructURL } from '@/utils/url'
 
 const STATUS_ICON_COLOR_MAP = {
@@ -190,7 +190,7 @@ const timelineData = computed(() => {
 
           <div v-if="run?.ip_version && run?.ip_address">
             <span class="mr-2">•</span>
-            <span>{{ IP_VERSION_LABELS[run?.ip_version as keyof typeof IP_VERSION_LABELS] }}{{ ':' }} {{ run?.ip_address }}</span>
+            <span>{{ run?.ip_version }}{{ ':' }} {{ run?.ip_address }}</span>
           </div>
         </div>
         <div class="flex items-center justify-between">
